b37c5985251325cc004203d3b71dcb551caa9473
itcottage <itcottage@67491823-2d96-4828-9f0b-4bae32b7e0f6>
2009-06-14 17:34:04 +0000

    updated authentication

--- fams/app/controllers/fams_controller.php
+++ fams/app/controllers/fams_controller.php
@@ -1,34 +1,15 @@
 <?php
-/* FILE Id: home_controller.php */
-
-/**
- * Short description for file.
- *
- * This file is application-wide controller file. You can put all
- * application-wide controller-related methods here.
- *
- * @filesource
- * @copyright     Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
- * @link          http://www.cakefoundation.org/projects/info/cakephp CakePHP(tm) Project
- * @package       cake
- * @subpackage    cake.cake.libs.controller
- * @since         CakePHP(tm) v 0.2.9
- * @version       $Revision: 7945 $
- * @modifiedby    $LastChangedBy: gwoo $
- * @lastmodified  $Date: 2008-12-18 20:16:01 -0600 (Thu, 18 Dec 2008) $
- * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
- */
-?>
-
-<?php
 class FamsController extends AppController {
 
 	var $name = 'Fams';
-
+	var $components = array('Auth');
 	var $uses = array('SystemMenu');
 	var $helpers = array('Html', 'Form', 'Javascript');
-
-	
+/*
+	function beforeFilter() {
+        $this->Auth->allow('index','view');
+	}
+*/
 	function index() {
 		
 	}
@@ -37,4 +18,4 @@ class FamsController extends AppController {
 
 	}
 }
-?> 
+?>
