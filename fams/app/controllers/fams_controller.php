2ad83a0dab1137de595f6b20ff4ef4c6a7a3cb64
itcottage <itcottage@67491823-2d96-4828-9f0b-4bae32b7e0f6>
2009-06-17 12:04:17 +0000

    updated

--- fams/app/controllers/fams_controller.php
+++ fams/app/controllers/fams_controller.php
@@ -5,17 +5,13 @@ class FamsController extends AppController {
 	var $components = array('Auth');
 	var $uses = array('SystemMenu');
 	var $helpers = array('Html', 'Form', 'Javascript');
-/*
-	function beforeFilter() {
-        $this->Auth->allow('index','view');
-	}
-*/
+
 	function index() {
 		
 	}
 	
 	function home() {
-
+		
 	}
 }
 ?>
