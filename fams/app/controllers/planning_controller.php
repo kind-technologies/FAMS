9ea9a30811411f665e782b16cd02a5085120a14e
itcottage <itcottage@67491823-2d96-4828-9f0b-4bae32b7e0f6>
2009-07-05 18:10:24 +0000

    updated

--- fams/app/controllers/planning_controller.php
+++ fams/app/controllers/planning_controller.php
@@ -4,10 +4,37 @@ class PlanningController extends AppController {
 	var $name = 'Planning';
 	var $components = array('Auth');
 	var $helpers = array('Html', 'Form', 'Javascript');
-	var $uses = array();
+	var $uses = array('Employee');
 	
 	function employees() {
+		Configure::write('debug', 0);
+	
+		$employees = $this->Employee->findAll();
+		
+		$emp_data = '[';
 
+		
+		foreach($employees as $employee) {
+			$emp_data .= '["'. $employee['Employee']['employee_id'] . 
+							'", "' . $employee['Employee']['name_with_initials'] .
+							'", "' . $employee['Employee']['contact_number'] .
+							'", "' . $employee['Employee']['branch_id'] .
+							'","' . $employee['Employee']['division_id'] .
+							'", "' . $employee['Employee']['date_of_birth'] .
+							'", "' . $employee['Employee']['full_name'] .
+							'", "' . $employee['Employee']['gender'] .
+							'", "' . $employee['Employee']['national_id'] .
+							'", "' . $employee['Employee']['address'] .
+							'", ' . $employee['Employee']['id'] .
+							'],';
+		}
+		
+		$emp_data = rtrim($emp_data, ",");
+		
+		$emp_data .= ']';
+		
+		$this->set('emp_data', $emp_data);
+		
 	}
 
 	function emplayee_update() {
