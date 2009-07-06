99be5cbc6743dd664541c22c71074e8a960141fc
itcottage <itcottage@67491823-2d96-4828-9f0b-4bae32b7e0f6>
2009-07-06 16:49:48 +0000

    updated

--- fams/app/controllers/planning_controller.php
+++ fams/app/controllers/planning_controller.php
@@ -4,13 +4,13 @@ class PlanningController extends AppController {
 	var $name = 'Planning';
 	var $components = array('Auth');
 	var $helpers = array('Html', 'Form', 'Javascript');
-	var $uses = array('Employee');
+	var $uses = array('Employee', 'Branch', 'Division');
 	
 	function employees() {
 		Configure::write('debug', 0);
 	
 		$employees = $this->Employee->findAll();
-		
+
 		$emp_data = '[';
 
 		
@@ -18,8 +18,8 @@ class PlanningController extends AppController {
 			$emp_data .= '["'. $employee['Employee']['employee_id'] . 
 							'", "' . $employee['Employee']['name_with_initials'] .
 							'", "' . $employee['Employee']['contact_number'] .
-							'", "' . $employee['Employee']['branch_id'] .
-							'","' . $employee['Employee']['division_id'] .
+							'", "' . $employee['Branch']['branch_code'] .
+							'","' . $employee['Division']['division_code'] .
 							'", "' . date( 'm/d/Y', strtotime($employee['Employee']['date_of_birth'])) .
 							'", "' . $employee['Employee']['full_name'] .
 							'", "' . $employee['Employee']['gender'] .
