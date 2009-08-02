753e47d91aca8631e8dcc6301feafa0f737dd63f
itcottage <itcottage@67491823-2d96-4828-9f0b-4bae32b7e0f6>
2009-08-02 05:44:04 +0000

    updated

--- fams/app/controllers/employees_controller.php
+++ fams/app/controllers/employees_controller.php
@@ -19,46 +19,57 @@ class EmployeesController extends AppController {
 	// Action to Add/Edit/Delete employee records via Ajax
 	function emplayee_update() {
 		Configure::write('debug', 0);
+		
 		$this->layout = 'ajax';
-	
-		$this->data['Employee']['employee_id'] = 
-											$this->params['form']['employee_id'];
-		$this->data['Employee']['full_name'] = 
-											$this->params['form']['full_name'];
-		$this->data['Employee']['name_with_initials'] = 
-											$this->params['form']['name_with_initials'];
-		$this->data['Employee']['date_of_birth'] = 
-											$this->params['form']['date_of_birth'];
 		
-		$this->data['Employee']['gender'] = 	$this->params['form']['gender'];
-		
-		$this->data['Employee']['national_id'] = 
-											$this->params['form']['national_id'];
-		$this->data['Employee']['address'] = 
-											$this->params['form']['address'];
-		$this->data['Employee']['contact_number'] = 
-											$this->params['form']['contact_number'];
-		$this->data['Employee']['branch_id'] = 
-											$this->params['form']['branch_id'];
-		$this->data['Employee']['division_id'] = 
-											$this->params['form']['division_id'];
-
 		$record_id = $this->params['form']['id'];
 		$form_action = $this->params['form']['action'];
+			
+		if($form_action == '__a' || $form_action == '__e') {
+			$this->data['Employee']['employee_id'] = 
+												$this->params['form']['employee_id'];
+			$this->data['Employee']['full_name'] = 
+												$this->params['form']['full_name'];
+			$this->data['Employee']['name_with_initials'] = 
+												$this->params['form']['name_with_initials'];
+			$this->data['Employee']['date_of_birth'] = 
+												$this->params['form']['date_of_birth'];
+		
+			$this->data['Employee']['gender'] = 	$this->params['form']['gender'];
+		
+			$this->data['Employee']['email'] = 
+												$this->params['form']['email'];
+			$this->data['Employee']['address'] = 
+												$this->params['form']['address'];
+			$this->data['Employee']['contact_number'] = 
+												$this->params['form']['contact_number'];
+			$this->data['Employee']['branch_id'] = 
+												$this->params['form']['branch_id'];
+			$this->data['Employee']['division_id'] = 
+												$this->params['form']['division_id'];
 
-		
-		if($form_action == '__a') { // If request is to ADD a new record
-			$this->data['Employee']['id'] == null;
-			$this->Employee->save($this->data);
-		
-		} elseif($form_action == '__e') { // If request is to EDIT a record		
-
-		
+			$this->data['Employee']['photo'] = '';
+			$this->data['Employee']['record_status'] = 'A'; // Record in Active status
+			
+								
+			if($form_action == '__a') { // If request is to ADD a new record
+			
+				$this->Employee->create();
+				$this->Employee->save($this->data);
+			
+			} elseif($form_action == '__e') { // If request is to EDIT a record		
+			
+				$this->data['Employee']['id'] = $record_id;
+				$this->Employee->save($this->data);
+			
+			}
 		} elseif($form_action == '__d') { // If request is to DELETE a record
-
-		
+			$this->data['Employee']['id'] = $record_id;
+			$this->data['Employee']['record_status'] = 'D'; // Record in Deleted status
+			$this->Employee->save($this->data);	
 		}
-//		$this->set('params', $this->params['form']);
+		
+		$this->set('params', $record_id . $form_action);
 
 		// Set data for update data-grid.
 		// JSON object is created in view file accordingly
