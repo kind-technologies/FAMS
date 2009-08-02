c654ce77b6852f923a1e5bb5c3ea8810d5eb1914
itcottage <itcottage@67491823-2d96-4828-9f0b-4bae32b7e0f6>
2009-08-02 05:43:07 +0000

    updated

--- fams/database/fams_db.sql
+++ fams/database/fams_db.sql
@@ -2,24 +2,6 @@
 -- PostgreSQL database dump
 --
 
--- Started on 2009-07-28 22:34:35 IST
-
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = off;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET escape_string_warning = off;
-
---
--- TOC entry 1812 (class 1262 OID 16385)
--- Name: fams; Type: DATABASE; Schema: -; Owner: -
---
-
-CREATE DATABASE fams WITH TEMPLATE = template0 ENCODING = 'UTF8';
-
-
-\connect fams
-
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = off;
 SET check_function_bodies = false;
@@ -33,8 +15,6 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 1490 (class 1259 OID 16396)
--- Dependencies: 1770 1771 6
 -- Name: acos; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -50,8 +30,6 @@ CREATE TABLE acos (
 
 
 --
--- TOC entry 1492 (class 1259 OID 16409)
--- Dependencies: 1773 1774 6
 -- Name: aros; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -67,8 +45,6 @@ CREATE TABLE aros (
 
 
 --
--- TOC entry 1494 (class 1259 OID 16422)
--- Dependencies: 1776 1777 1778 1779 6
 -- Name: aros_acos; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -84,8 +60,6 @@ CREATE TABLE aros_acos (
 
 
 --
--- TOC entry 1500 (class 1259 OID 16464)
--- Dependencies: 6
 -- Name: branches; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -97,8 +71,6 @@ CREATE TABLE branches (
 
 
 --
--- TOC entry 1502 (class 1259 OID 16472)
--- Dependencies: 6
 -- Name: divisions; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -110,8 +82,6 @@ CREATE TABLE divisions (
 
 
 --
--- TOC entry 1498 (class 1259 OID 16453)
--- Dependencies: 6
 -- Name: employees; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -122,17 +92,17 @@ CREATE TABLE employees (
     name_with_initials character varying(50) NOT NULL,
     date_of_birth date,
     gender character(1),
-    national_id character varying(20),
+    email character varying(50),
     address character varying(300),
     contact_number character varying(20),
     branch_id integer,
-    division_id integer
+    division_id integer,
+    photo character varying(50),
+    record_status character(1)
 );
 
 
 --
--- TOC entry 1487 (class 1259 OID 16386)
--- Dependencies: 6
 -- Name: system_menus; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -147,8 +117,6 @@ CREATE TABLE system_menus (
 
 
 --
--- TOC entry 1496 (class 1259 OID 16435)
--- Dependencies: 6
 -- Name: users; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -160,8 +128,6 @@ CREATE TABLE users (
 
 
 --
--- TOC entry 1489 (class 1259 OID 16394)
--- Dependencies: 6 1490
 -- Name: acos_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -174,8 +140,6 @@ CREATE SEQUENCE acos_id_seq
 
 
 --
--- TOC entry 1815 (class 0 OID 0)
--- Dependencies: 1489
 -- Name: acos_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -183,8 +147,6 @@ ALTER SEQUENCE acos_id_seq OWNED BY acos.id;
 
 
 --
--- TOC entry 1816 (class 0 OID 0)
--- Dependencies: 1489
 -- Name: acos_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
@@ -192,8 +154,6 @@ SELECT pg_catalog.setval('acos_id_seq', 1, false);
 
 
 --
--- TOC entry 1493 (class 1259 OID 16420)
--- Dependencies: 1494 6
 -- Name: aros_acos_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -206,8 +166,6 @@ CREATE SEQUENCE aros_acos_id_seq
 
 
 --
--- TOC entry 1817 (class 0 OID 0)
--- Dependencies: 1493
 -- Name: aros_acos_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -215,8 +173,6 @@ ALTER SEQUENCE aros_acos_id_seq OWNED BY aros_acos.id;
 
 
 --
--- TOC entry 1818 (class 0 OID 0)
--- Dependencies: 1493
 -- Name: aros_acos_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
@@ -224,8 +180,6 @@ SELECT pg_catalog.setval('aros_acos_id_seq', 1, false);
 
 
 --
--- TOC entry 1491 (class 1259 OID 16407)
--- Dependencies: 6 1492
 -- Name: aros_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -238,8 +192,6 @@ CREATE SEQUENCE aros_id_seq
 
 
 --
--- TOC entry 1819 (class 0 OID 0)
--- Dependencies: 1491
 -- Name: aros_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -247,8 +199,6 @@ ALTER SEQUENCE aros_id_seq OWNED BY aros.id;
 
 
 --
--- TOC entry 1820 (class 0 OID 0)
--- Dependencies: 1491
 -- Name: aros_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
@@ -256,8 +206,6 @@ SELECT pg_catalog.setval('aros_id_seq', 1, false);
 
 
 --
--- TOC entry 1499 (class 1259 OID 16462)
--- Dependencies: 6 1500
 -- Name: branches_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -269,8 +217,6 @@ CREATE SEQUENCE branches_id_seq
 
 
 --
--- TOC entry 1821 (class 0 OID 0)
--- Dependencies: 1499
 -- Name: branches_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -278,8 +224,6 @@ ALTER SEQUENCE branches_id_seq OWNED BY branches.id;
 
 
 --
--- TOC entry 1822 (class 0 OID 0)
--- Dependencies: 1499
 -- Name: branches_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
@@ -287,8 +231,6 @@ SELECT pg_catalog.setval('branches_id_seq', 2, true);
 
 
 --
--- TOC entry 1501 (class 1259 OID 16470)
--- Dependencies: 6 1502
 -- Name: divisions_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -300,8 +242,6 @@ CREATE SEQUENCE divisions_id_seq
 
 
 --
--- TOC entry 1823 (class 0 OID 0)
--- Dependencies: 1501
 -- Name: divisions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -309,8 +249,6 @@ ALTER SEQUENCE divisions_id_seq OWNED BY divisions.id;
 
 
 --
--- TOC entry 1824 (class 0 OID 0)
--- Dependencies: 1501
 -- Name: divisions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
@@ -318,8 +256,6 @@ SELECT pg_catalog.setval('divisions_id_seq', 2, true);
 
 
 --
--- TOC entry 1497 (class 1259 OID 16451)
--- Dependencies: 1498 6
 -- Name: employees_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -331,8 +267,6 @@ CREATE SEQUENCE employees_id_seq
 
 
 --
--- TOC entry 1825 (class 0 OID 0)
--- Dependencies: 1497
 -- Name: employees_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -340,17 +274,13 @@ ALTER SEQUENCE employees_id_seq OWNED BY employees.id;
 
 
 --
--- TOC entry 1826 (class 0 OID 0)
--- Dependencies: 1497
 -- Name: employees_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('employees_id_seq', 12, true);
+SELECT pg_catalog.setval('employees_id_seq', 15, true);
 
 
 --
--- TOC entry 1488 (class 1259 OID 16389)
--- Dependencies: 6 1487
 -- Name: system_menu_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -362,8 +292,6 @@ CREATE SEQUENCE system_menu_id_seq
 
 
 --
--- TOC entry 1827 (class 0 OID 0)
--- Dependencies: 1488
 -- Name: system_menu_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -371,8 +299,6 @@ ALTER SEQUENCE system_menu_id_seq OWNED BY system_menus.id;
 
 
 --
--- TOC entry 1828 (class 0 OID 0)
--- Dependencies: 1488
 -- Name: system_menu_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
@@ -380,8 +306,6 @@ SELECT pg_catalog.setval('system_menu_id_seq', 19, true);
 
 
 --
--- TOC entry 1495 (class 1259 OID 16433)
--- Dependencies: 6 1496
 -- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -393,8 +317,6 @@ CREATE SEQUENCE users_id_seq
 
 
 --
--- TOC entry 1829 (class 0 OID 0)
--- Dependencies: 1495
 -- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -402,8 +324,6 @@ ALTER SEQUENCE users_id_seq OWNED BY users.id;
 
 
 --
--- TOC entry 1830 (class 0 OID 0)
--- Dependencies: 1495
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
@@ -411,8 +331,6 @@ SELECT pg_catalog.setval('users_id_seq', 1, true);
 
 
 --
--- TOC entry 1772 (class 2604 OID 16486)
--- Dependencies: 1490 1489 1490
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -420,8 +338,6 @@ ALTER TABLE acos ALTER COLUMN id SET DEFAULT nextval('acos_id_seq'::regclass);
 
 
 --
--- TOC entry 1775 (class 2604 OID 16487)
--- Dependencies: 1491 1492 1492
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -429,8 +345,6 @@ ALTER TABLE aros ALTER COLUMN id SET DEFAULT nextval('aros_id_seq'::regclass);
 
 
 --
--- TOC entry 1780 (class 2604 OID 16488)
--- Dependencies: 1493 1494 1494
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -438,8 +352,6 @@ ALTER TABLE aros_acos ALTER COLUMN id SET DEFAULT nextval('aros_acos_id_seq'::re
 
 
 --
--- TOC entry 1783 (class 2604 OID 16489)
--- Dependencies: 1499 1500 1500
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -447,8 +359,6 @@ ALTER TABLE branches ALTER COLUMN id SET DEFAULT nextval('branches_id_seq'::regc
 
 
 --
--- TOC entry 1784 (class 2604 OID 16490)
--- Dependencies: 1502 1501 1502
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -456,8 +366,6 @@ ALTER TABLE divisions ALTER COLUMN id SET DEFAULT nextval('divisions_id_seq'::re
 
 
 --
--- TOC entry 1782 (class 2604 OID 16491)
--- Dependencies: 1498 1497 1498
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -465,8 +373,6 @@ ALTER TABLE employees ALTER COLUMN id SET DEFAULT nextval('employees_id_seq'::re
 
 
 --
--- TOC entry 1769 (class 2604 OID 16492)
--- Dependencies: 1488 1487
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -474,8 +380,6 @@ ALTER TABLE system_menus ALTER COLUMN id SET DEFAULT nextval('system_menu_id_seq
 
 
 --
--- TOC entry 1781 (class 2604 OID 16493)
--- Dependencies: 1496 1495 1496
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -483,8 +387,6 @@ ALTER TABLE users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
 
 
 --
--- TOC entry 1803 (class 0 OID 16396)
--- Dependencies: 1490
 -- Data for Name: acos; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -493,8 +395,6 @@ COPY acos (id, parent_id, model, foreign_key, alias, lft, rght) FROM stdin;
 
 
 --
--- TOC entry 1804 (class 0 OID 16409)
--- Dependencies: 1492
 -- Data for Name: aros; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -503,8 +403,6 @@ COPY aros (id, parent_id, model, foreign_key, alias, lft, rght) FROM stdin;
 
 
 --
--- TOC entry 1805 (class 0 OID 16422)
--- Dependencies: 1494
 -- Data for Name: aros_acos; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -513,8 +411,6 @@ COPY aros_acos (id, aro_id, aco_id, _create, _read, _update, _delete) FROM stdin
 
 
 --
--- TOC entry 1808 (class 0 OID 16464)
--- Dependencies: 1500
 -- Data for Name: branches; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -525,8 +421,6 @@ COPY branches (id, branch_code, description) FROM stdin;
 
 
 --
--- TOC entry 1809 (class 0 OID 16472)
--- Dependencies: 1502
 -- Data for Name: divisions; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -537,30 +431,28 @@ COPY divisions (id, division_code, description) FROM stdin;
 
 
 --
--- TOC entry 1807 (class 0 OID 16453)
--- Dependencies: 1498
 -- Data for Name: employees; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY employees (id, employee_id, full_name, name_with_initials, date_of_birth, gender, national_id, address, contact_number, branch_id, division_id) FROM stdin;
-1	00001	Chaminda Keerthi Bandara	MMCK Bandara	1981-04-11	M	811021938V	113/4, Kithulawila Uyana, Kiriwattuduwa	94716833516	2	2
-4	00004	Darshika Weerasinghe	DD Weerasinghe	1980-08-21	F	811021938V	\N	94716833516	2	2
-5	00005	Dihan Praneeth	D Praneeth	1965-09-12	M	811021938V	\N	94716833516	1	1
-6	00006	Nishan Pradeep	N Pradeep	1974-08-15	M	811021938V	\N	94716833516	2	1
-7	00007	Nishchala Kodithuwakku	N Kodithuwakku	1983-05-13	F	811021938V	\N	94716833516	2	2
-2	00002	Nuwan Chaturanga	MN Chaturanga	1981-05-23	M	811021938V	\N	94714567567	2	2
-3	00003	James Cluff	J Cluff	1955-03-30	M	551021938V	\N	01716833516	1	1
-8	00008	Dewian Cluff	D Cluff	1965-03-19	M	97654321V	Main Street, St George, Utah USA	0123456789	\N	\N
-9	00008	Abigail Cluff	A Cluff	1991-07-19	F	97654321V	Main Street, St George, Utah USA	0123456789	\N	\N
-10	00008	Abigail Cluff	A Cluff	1991-07-19	F	97654321V	Main Street, St George, Utah USA	0123456789	\N	\N
-11	00010	Abigail Cluff	A Cluff	1991-07-19	F	97654321V	Main Street, St George, Utah USA	0123456789	1	1
-12	00011	Mali Duruge	D Duruge	2009-07-08	F	09343434V	Thalahena North,	7101223123	2	2
+COPY employees (id, employee_id, full_name, name_with_initials, date_of_birth, gender, email, address, contact_number, branch_id, division_id, photo, record_status) FROM stdin;
+14	00014	Mohomad Huzzain	M Huzzain	1985-02-04	M	mohomad.huzzain@laknipayum.com	Main Street, Colombo 08	0781234567	2	2		D
+12	00011	Mali Duruge	M Duruge	2009-07-08	F	keerthi.bandara@laknipayum.com	Thalahena North,	7101223123	2	2		A
+4	00004	Darshika Weerasinghe	DD Weerasinghe	1980-08-21	F	keerthi.bandara@laknipayum.com	\N	94716833516	2	2	\N	A
+5	00005	Dihan Praneeth	D Praneeth	1965-09-12	M	keerthi.bandara@laknipayum.com	\N	94716833516	1	1	\N	A
+6	00006	Nishan Pradeep	N Pradeep	1974-08-15	M	keerthi.bandara@laknipayum.com	\N	94716833516	2	1	\N	A
+13	00013	Asanka Indika	A Indika	1975-08-04	M	asanka.indika@laknipayum.com	Galle Road, Ambalangoda	0721234567	2	2	\N	A
+11	00010	Abigail Cluff	A Cluff	1991-07-19	F	keerthi.bandara@laknipayum.com	Main Street, St George, Utah USA	0123456789	1	1	\N	A
+7	00007	Nishchala Kodithuwakku	N Kodithuwakku	1983-05-13	F	keerthi.bandara@laknipayum.com	\N	94716833516	2	2	\N	A
+10	00008	Abigail Cluff	A Cluff	1991-07-19	F	keerthi.bandara@laknipayum.com	Main Street, St George, Utah USA	0123456789	\N	\N	\N	D
+9	00008	Abigail Cluff	A Cluff	1991-07-19	F	keerthi.bandara@laknipayum.com	Main Street, St George, Utah USA	0123456789	\N	\N	\N	D
+8	00008	Dewian Cluff	D Cluff	1965-03-19	M	keerthi.bandara@laknipayum.com	Main Street, St George, Utah USA	0123456789	\N	\N	\N	D
+2	00002	Nuwan Chaturanga	MN Chaturanga	1981-05-23	M	nuwan.chaturanga@laknipayum.com	Pitipana, Balangoda.	94714567567	2	2		A
+3	00003	James Cluff	J Cluff	1955-03-30	M	james.cluff@laknipayum.com	St. George, Utah	01716833516	1	1		A
+1	00001	Chaminda Keerthi Bandara	MMCK Bandara	1981-04-11	M	keerthi.bandara@laknipayum.com	113/4, Kithulawila Uyana, Kiriwattuduwa	0716833516	2	2		A
 \.
 
 
 --
--- TOC entry 1802 (class 0 OID 16386)
--- Dependencies: 1487
 -- Data for Name: system_menus; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -588,8 +480,6 @@ COPY system_menus (id, parent_id, title, description, program_name, "order") FRO
 
 
 --
--- TOC entry 1806 (class 0 OID 16435)
--- Dependencies: 1496
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -599,8 +489,6 @@ COPY users (id, username, password) FROM stdin;
 
 
 --
--- TOC entry 1788 (class 2606 OID 16406)
--- Dependencies: 1490 1490
 -- Name: acos_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -609,8 +497,6 @@ ALTER TABLE ONLY acos
 
 
 --
--- TOC entry 1793 (class 2606 OID 16431)
--- Dependencies: 1494 1494
 -- Name: aros_acos_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -619,8 +505,6 @@ ALTER TABLE ONLY aros_acos
 
 
 --
--- TOC entry 1790 (class 2606 OID 16419)
--- Dependencies: 1492 1492
 -- Name: aros_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -629,8 +513,6 @@ ALTER TABLE ONLY aros
 
 
 --
--- TOC entry 1799 (class 2606 OID 16469)
--- Dependencies: 1500 1500
 -- Name: branches_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -639,8 +521,6 @@ ALTER TABLE ONLY branches
 
 
 --
--- TOC entry 1801 (class 2606 OID 16477)
--- Dependencies: 1502 1502
 -- Name: divisions_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -649,8 +529,6 @@ ALTER TABLE ONLY divisions
 
 
 --
--- TOC entry 1797 (class 2606 OID 16458)
--- Dependencies: 1498 1498
 -- Name: employees_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -659,8 +537,6 @@ ALTER TABLE ONLY employees
 
 
 --
--- TOC entry 1786 (class 2606 OID 16393)
--- Dependencies: 1487 1487
 -- Name: system_menu_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -669,8 +545,6 @@ ALTER TABLE ONLY system_menus
 
 
 --
--- TOC entry 1795 (class 2606 OID 16440)
--- Dependencies: 1496 1496
 -- Name: users_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -679,8 +553,6 @@ ALTER TABLE ONLY users
 
 
 --
--- TOC entry 1791 (class 1259 OID 16432)
--- Dependencies: 1494 1494
 -- Name: aro_aco_key; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -688,8 +560,6 @@ CREATE UNIQUE INDEX aro_aco_key ON aros_acos USING btree (aro_id, aco_id);
 
 
 --
--- TOC entry 1814 (class 0 OID 0)
--- Dependencies: 6
 -- Name: public; Type: ACL; Schema: -; Owner: -
 --
 
@@ -698,8 +568,6 @@ REVOKE ALL ON SCHEMA public FROM postgres;
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
-
--- Completed on 2009-07-28 22:34:35 IST
 
 --
 -- PostgreSQL database dump complete
