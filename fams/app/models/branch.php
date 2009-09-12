d790bd0eb9bfc14613cdb216a66f6ee7e6004f76
itcottage <itcottage@67491823-2d96-4828-9f0b-4bae32b7e0f6>
2009-09-12 08:29:31 +0000

    updated

--- fams/app/models/branch.php
+++ fams/app/models/branch.php
@@ -3,5 +3,19 @@ class Branch extends AppModel {
 
 	var $name = 'Branch';
 
+	function get_branches_for_json() {
+		$branches = $this->findAll(null, null, 'Branch.id ASC');
+		$branches_data = array();
+
+		foreach($branches as $branch) {
+
+			$branches_data[] = array($branch['Branch']['id'], 
+									$branch['Branch']['branch_code'], 
+									$branch['Branch']['description'] );
+			
+		}
+		
+		return $branches_data;
+	}
 }
 ?>
--- fams/app/models/division.php
+++ fams/app/models/division.php
@@ -2,6 +2,20 @@
 class Division extends AppModel {
 
 	var $name = 'Division';
+	
+	function get_divisions_for_json() {
+		$divisions = $this->findAll(null, null, 'Division.id ASC');
+		$divisions_data = array();
 
+		foreach($divisions as $division) {
+
+			$divisions_data[] = array($division['Division']['id'], 
+									$division['Division']['division_code'], 
+									$division['Division']['description'] );
+			
+		}
+		
+		return $divisions_data;
+	}
 }
 ?>
