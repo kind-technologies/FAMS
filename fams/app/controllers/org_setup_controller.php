cac2a9c5152a2f39db085806b3541cfff3b18b9b
itcottage <itcottage@67491823-2d96-4828-9f0b-4bae32b7e0f6>
2009-09-12 08:28:49 +0000

    updated

---
+++ fams/app/controllers/org_setup_controller.php
@@ -0,0 +1,27 @@
+<?php
+class OrgSetupController extends AppController {
+
+	var $name = 'OrgSetup';
+	var $uses = array('Branch', 'Division');
+	
+	function org_structure() {
+		Configure::write('debug', 0);
+		$this->redirect('branch_view');
+	}
+	
+	function branch_view() {
+		Configure::write('debug', 0);
+
+		$branches_data = $this->Branch->get_branches_for_json();
+		$this->set('branches_data', array('branches_data' => $branches_data));	
+	}
+	
+	function division_view() {
+		//Configure::write('debug', 0);
+
+		$divisions_data = $this->Division->get_divisions_for_json();
+		$this->set('divisions_data', array('divisions_data' => $divisions_data));	
+	}
+
+}
+?>
