f160965a5f2c8efd6c71e39bfdd561807823717d
itcottage <itcottage@67491823-2d96-4828-9f0b-4bae32b7e0f6>
2009-09-16 11:18:08 +0000

    updated

--- fams/app/tests/cases/models/branch.test.php
+++ fams/app/tests/cases/models/branch.test.php
@@ -3,6 +3,16 @@
 
    
    class BranchTestCase extends CakeTestCase { 
-          var $fixtures = array( 'app.branch' ); 
+
+		var $fixtures = array( 'app.branch' );
+          
+		function testPublished() {
+			$this->Branch =& ClassRegistry::init('Branch');
+
+			$result = $this->Branch->get_branches_for_json();
+
+			$this->assertNotNull($result);
+		}
+
    } 
  ?> 
