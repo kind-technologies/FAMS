ef459828973f2d410bec583015fb6d18cfbf1999
itcottage <itcottage@67491823-2d96-4828-9f0b-4bae32b7e0f6>
2009-10-16 07:31:28 +0000

    updated

--- fams/app/controllers/suppliers_controller.php
+++ fams/app/controllers/suppliers_controller.php
@@ -19,42 +19,49 @@ class SuppliersController extends AppController {
 		Configure::write('debug', 0);
 		
 		$this->layout = 'ajax';
-/*	
+	
 		$record_id = $this->params['form']['id'];
 		$form_action = $this->params['form']['action'];
 			
 		if($form_action == '__a' || $form_action == '__e') {
-			$this->data['Branch']['branch_code'] = 
-												$this->params['form']['branch_code'];
-			$this->data['Branch']['description'] = 
-												$this->params['form']['branch_description'];
-			$this->data['Branch']['record_status'] = 'A'; // Record in Active status
+			$this->data['Supplier']['supplier_code'] = 
+												$this->params['form']['supplier_code'];
+			$this->data['Supplier']['description'] = 
+												$this->params['form']['description'];
+			$this->data['Supplier']['address'] = 
+												$this->params['form']['address'];
+			$this->data['Supplier']['contact_number'] = 
+												$this->params['form']['contact_number'];
+			$this->data['Supplier']['email'] = 
+												$this->params['form']['email'];
+
+			$this->data['Supplier']['record_status'] = 'A'; // Record in Active status
 			
 								
 			if($form_action == '__a') { // If request is to ADD a new record
 			
-				$this->Branch->create();
-				$this->Branch->save($this->data);
+				$this->Supplier->create();
+				$this->Supplier->save($this->data);
 			
 			} elseif($form_action == '__e') { // If request is to EDIT a record		
 			
-				$this->data['Branch']['id'] = $record_id;
-				$this->Branch->save($this->data);
+				$this->data['Supplier']['id'] = $record_id;
+				$this->Supplier->save($this->data);
 			
 			}
 		} elseif($form_action == '__d') { // If request is to DELETE a record
-			$this->data['Branch']['id'] = $record_id;
-			$this->data['Branch']['record_status'] = 'D'; // Record in Deleted status
-			$this->Branch->save($this->data);	
+			$this->data['Supplier']['id'] = $record_id;
+			$this->data['Supplier']['record_status'] = 'D'; // Record in Deleted status
+			$this->Supplier->save($this->data);	
 		}
 		
 		//$this->set('params', $record_id . $form_action);
 
 		// Set data for update data-grid.
 		// JSON object is created in view file accordingly
-		$branch_data = $this->Branch->get_branches_for_json();
-		$this->set('branch_data', $branch_data);
-*/	
+		$suppliers_data = $this->Supplier->get_suppliers_for_json();
+		$this->set('suppliers_data', $suppliers_data);
+	
 	}
 
 
