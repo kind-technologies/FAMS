aad6f9c80860ccb25f015dc54303b95893c13cf1
itcottage <itcottage@67491823-2d96-4828-9f0b-4bae32b7e0f6>
2009-10-23 12:11:50 +0000

    updated

--- fams/app/controllers/components/depreciation.php
+++ fams/app/controllers/components/depreciation.php
@@ -6,7 +6,7 @@ class DepreciationComponent extends Object {
 	var $salvage_value = 0;
 	var $lifespan = 0;
 	var $asset_commencement_year = 0;
-
+	var $asset_commencement_month = '';
 
 	// Method to caluculate anual depreciation
     function get_annual_depreciation() {
@@ -23,6 +23,28 @@ class DepreciationComponent extends Object {
     function get_net_book_value() {
     //org cost - cur dep
     		return number_format(($this->purchase_price - $this->get_total_depreciation()), 2);
+    }
+    
+    function get_depr_chart_data() {
+		
+		$number_years_in_use = ($this->lifespan) / 12;
+		$chart_data = array();
+		
+		$depreciation_amt = $this->get_annual_depreciation();
+		
+		for($i= 0; $i < $number_years_in_use; $i++) {
+			$year_value = $this->asset_commencement_month . 
+									' ' . ($this->asset_commencement_year + $i);
+			$accumulate_val  = $depreciation_amt + ($depreciation_amt * $i);
+			$depre_val = ($this->purchase_price) - $accumulate_val;
+		
+			$chart_data[] = array($year_value, $accumulate_val, $depre_val); 		
+		}
+
+		//$chart_data[] = array('Jul 08', 6000, 14000); 
+		//$chart_data[] = array('Jul 09', 9000, 11000); 
+
+		return $chart_data;
     }
 }
 
